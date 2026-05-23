9a606cb5c944850742a21e829a99887047375c6d
Robmel <elrobmon@gmail.com>
2026-05-23 18:07:42 -0400

    Agregar Metodo Validar tipo habitacion

---
+++ HotelZormat.Negocio/Servicios/ReservaService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelZormat.Negocio.Servicios
+{
+    // TODO:
+    public class ReservaService
+    {
+        /// <summary>
+        /// Verifica si el tipo de habitación es uno de los permitidos.
+        /// Reto 01 · usa if / else
+        /// /// </summary>
+        public bool ValidarTipoHabitacion(string tipo)
+        {
+            if (string.IsNullOrWhiteSpace(tipo)) return false;
+            return tipo == "Sencilla" || tipo == "Doble" || tipo == "Suite";
+        }
+    }
+}
