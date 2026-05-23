2724219d40be385693374515717536c6865b43f2
Robmel <elrobmon@gmail.com>
2026-05-23 17:43:02 -0400

    Agregar archivos de proyecto.

---
+++ HotalZormat.Datos/Conexion/ConexionBD.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotalZormat.Datos.Conexion
+{
+    public static class ConexionBD
+    {
+        private static readonly string _cadena =
+            "Data source=.;Initial Catalog=HotelZormatDB;Integrated Security=true;";
+        public static SqlConnection ObtenerConexion()
+        {
+            return new SqlConnection(_cadena);
+        }
+    }
+}
